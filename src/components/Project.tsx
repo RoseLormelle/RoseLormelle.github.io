b8f1ce7db3198d8563e90ad0a560153281912210
Louis E <enjules31@gmail.com>
2026-01-02 16:28:43 +0100

    Feat : routing avec HashRouter

---
+++ src/components/Project.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const Project = () => {
+  return (
+    <div>
+      <h1>Project</h1>
+      <Link to="/">Retour à Home</Link>
+    </div>
+  );
+};
+
+export default Project;
+
